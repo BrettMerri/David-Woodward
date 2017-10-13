1dfb8531c3f2c6a911aeb62c2e68c1b6ddcc2686
BrettMerri <the.b.merri@gmail.com>
2017-10-12 22:14:37 -0400

    changed background image to a picture of david.

--- client/src/components/App/Main/Home/Home.js
+++ client/src/components/App/Main/Home/Home.js
@@ -8,18 +8,16 @@ const Home = () => (
         <div className="header-content-inner">
           <h1 className="title">David J. Woodward Scholarship Fund</h1>
           <p>established by Tim and Christine Woodward in memory of their son David.</p>
-          <button className="btn btn-default">Donate</button>
+          <button className="btn btn-default">Contribute</button>
         </div>
       </div>
     </header>
-    <div className="main">
+    <div className="main container">
       <div className="row section">
-        <div className="col">
+        <div className="col-md-6">
           <p>The David J. Woodward Scholarship Fund was established by Tim and Christine Woodward in memory of their son David. Applicants must have career goal of becoming a certified General Motors Technician (Automotive Service Education Program). The scholarship will be awarded to a student with a financial need who displays a commitment to serving others (in school, their community, or at home), a strong work ethic, and leadership skills associated to the automotive field.</p>
         </div>
-      </div>
-      <div className="row section">
-        <div className="col">
+        <div className="col-md-6">
           <p>The David J. Woodward Scholarship Fund is a 501(c)(3) organization dedicated helping those with a financial need by providing scholarships to deserving, qualified students with a strong desire to attend the GM A.S.E.P program and/or a current Warren Mott Football player that will attend a Vocational School, Trade or Technical school with the desire and commitment to become a Plumber, Electrician, or  H.V.A.C Technician.</p>
         </div>
       </div>
