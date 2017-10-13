c667811f27883ce528ce5693948440957906cb31
BrettMerri <the.b.merri@gmail.com>
2017-10-12 22:26:14 -0400

    saved placehold.it images to local

--- client/src/components/App/Main/About/About.js
+++ client/src/components/App/Main/About/About.js
@@ -9,7 +9,7 @@ const About = () => (
         <p>David James Woodward was born December 1, 1995 in Royal Oak, MI. From a young age he was an avid NASCAR Fan. He enjoyed watching races on TV and attending them when he could. He joined Cub Scouts because he got to build a Pinewood Derby Car to race. After a successful Cub Scout career and a few wins on the track he crossed over to Boy Scouts.</p>
       </div>
       <div className="col-md-4 flex-first">
-        <img className="full-width" src="http://placehold.it/400x300" alt="" />
+        <img className="full-width" src="/assets/img/400x300.png" alt="" />
       </div>
     </div>
     <div className="row section">
@@ -17,7 +17,7 @@ const About = () => (
         <p>In Boy Scouts, he loved camping, building fires and exploring nature. David was quiet and led by example. He was very focused on helping others in scouts, his neighborhood, and his community. After earning all the requirements for Eagle Scout and completing his Eagle Scout Project he was awarded the Eagle Scout Award.</p>
       </div>
       <div className="col-md-4 flex-last">
-        <img className="full-width" src="http://placehold.it/400x300" alt="" />
+        <img className="full-width" src="/assets/img/400x300.png" alt="" />
       </div>
     </div>
     <div className="row section">
@@ -25,7 +25,7 @@ const About = () => (
         <p>In High School, David played 4 years of football at Warren Mott. A sport in which he loved. In his senior year he was offered a job at Mike Savoie Chevrolet. He was hired as a lube rack technician.</p>
       </div>
       <div className="col-md-4 flex-first">
-        <img className="full-width" src="http://placehold.it/400x300" alt="" />
+        <img className="full-width" src="/assets/img/400x300.png" alt="" />
       </div>
     </div>
     <div className="row section">
@@ -33,7 +33,7 @@ const About = () => (
         <p>From his dedication, commitment and thirst for knowledge, he was offered an opportunity to attend the Macomb Community College - General Motors - Automotive Service Education Program (A.S.E.P). After 2 years of intense training he became a certified G.M. Technician. His love of cars, tinkering with motors of all kinds and helping others when asked, made David the kind of son his parents were very proud of.</p>
       </div>
       <div className="col-md-4 flex-last">
-        <img className="full-width" src="http://placehold.it/400x300" alt="" />
+        <img className="full-width" src="/assets/img/400x300.png" alt="" />
       </div>
     </div>
     <div className="row section">
@@ -41,7 +41,7 @@ const About = () => (
         <p>David died in a horrific accident on August 20, 2017. In his short 21 years, he touched many lives, from helping neighbors, friends and family. He loved to work, but also hang out and enjoy his friends. He lived life to it’s fullest, always had a smile on his face and was always up for a good “prank”.</p>
       </div>
       <div className="col-md-4 flex-first">
-        <img className="full-width" src="http://placehold.it/400x300" alt="" />
+        <img className="full-width" src="/assets/img/400x300.png" alt="" />
       </div>
     </div>
     <div className="row section">
@@ -49,7 +49,7 @@ const About = () => (
         <p>David's death has left a huge hole in our lives. Myself, His mother, and sister thought what better way to memorialize David’s Memory than to start a scholarship fund in his name. This fund is designed to assist those that have the same dream as David. The Scholarship will financially help those who want to become an automotive service technician. A smaller scholarship will also be handed out to a football player at Warren Mott High School that attends at Vocational, Trade, or Technical School with the desire and commitment to become a Plumber, Electrician, or H.V.A.C technician.</p>
       </div>
       <div className="col-md-4 flex-last">
-        <img className="full-width" src="http://placehold.it/400x300" alt="" />
+        <img className="full-width" src="/assets/img/400x300.png" alt="" />
       </div>
     </div>
   </div>
